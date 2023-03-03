59f21921700d02a6869bf7a79da83535eccb9644
Javier Martin <javiermartin@jmrobotica.com>
2023-03-03 12:39:32 +0100

    add new features

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var msg = 'Hola lcco mundo ptra vez';
+var msg = 'Hola ldffcco mdfundo ptra vez';
 var msg2 = 'Hola luego tambiccen';
+var warning = 'esto es una aviso'
+var warning2 = 'new branch'
 console.log(msg);
+console.log(warning2)
